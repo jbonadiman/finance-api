6c36cca0ca49b10a40c997855ede481911195c3f
João Vitor Bonadiman <j.bonadiman@live.com>
2021-01-22 01:49:33 -0300

    Refactor of finance function;

--- api/finance.go
+++ api/finance.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/jbonadiman/finance-bot/databases/redis"
 	"github.com/jbonadiman/finance-bot/utils"
@@ -32,69 +33,89 @@ type Task struct {
 }
 
 func GetTasks(w http.ResponseWriter, r *http.Request) {
-	taskListId, err := utils.LoadVar("TASK_LIST_ID")
+	token, err := getToken()
+	if err != nil  {
+		if err.Error() == "user not authenticated" {
+			log.Println("User not authenticated, redirecting to the login page")
+			http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+			return
+		}
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tasks, err := getTasks(token)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	ctx := context.Background()
-
-	redisClient, err := redis.New().GetClient()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	token := redisClient.Get(ctx, "token").Val()
-	if token == "" {
-		log.Println("User not authenticated, redirecting to the login page")
-
-		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
-		return
-	}
-
-	tasksUrl := fmt.Sprintf(TodoTasksUrl, taskListId)
-
-	req, err := http.NewRequest("GET", tasksUrl, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var tasks taskList
-
-	err = json.NewDecoder(resp.Body).Decode(&tasks)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = resp.Body.Close()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	for i := range tasks.Value {
-		fixTimeZone(&tasks.Value[i])
-	}
-
-	content, err := json.Marshal(tasks.Value)
+	content, err := json.Marshal(tasks)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.Write(content)
+}
+
+func getTasks(token string) (*[]Task, error) {
+	taskListId, err := utils.LoadVar("TASK_LIST_ID")
+	if err != nil {
+		return nil, err
+	}
+
+	tasksUrl := fmt.Sprintf(TodoTasksUrl, taskListId)
+
+	req, err := http.NewRequest("GET", tasksUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var tasks taskList
+
+	err = json.NewDecoder(resp.Body).Decode(&tasks)
+	if err != nil {
+		return nil, err
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range tasks.Value {
+		fixTimeZone(&tasks.Value[i])
+	}
+
+	return &tasks.Value, nil
+}
+
+func getToken() (string, error) {
+	ctx := context.Background()
+
+	redisClient, err := redis.New().GetClient()
+	if err != nil {
+		log.Printf("Error creating Redis client: %v\n", err)
+		return "", err
+	}
+
+	token := redisClient.Get(ctx, "token").Val()
+	if token == "" {
+		log.Println("User not authenticated!")
+
+		return "", errors.New("user not authenticated")
+	}
+
+	return token, nil
 }
 
 func fixTimeZone(task *Task) {
