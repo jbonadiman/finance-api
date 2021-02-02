5e78e474df65be3f4f0c7545b67dcf29ece27884
João Vitor Bonadiman <j.bonadiman@live.com>
2021-02-02 19:37:25 -0300

    Added environment module to load env vars;

---
+++ environment/environment.go
@@ -0,0 +1,113 @@
+package environment
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+)
+
+var doOnce sync.Once
+
+var (
+	MSClientID     string
+	MSClientSecret string
+	MSRedirectURL  string
+)
+
+var (
+	LambdaHost string
+	LambdaPassword  string
+	LambdaPort      string
+)
+
+var (
+	MongoHost     string
+	MongoPassword string
+	MongoUser     string
+)
+
+func init() {
+	loadMicrosoftVars()
+	loadLambdaStoreVars()
+}
+
+func loadLambdaStoreVars() {
+	var err error
+
+	doOnce.Do(func() {
+		log.Println("loading lambda store environment variables...")
+
+		LambdaHost, err = loadVar("LAMBDA_HOST")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		LambdaPassword, err = loadVar("LAMBDA_SECRET")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		LambdaPort, err = loadVar("LAMBDA_PORT")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	})
+}
+
+func loadMicrosoftVars() {
+	var err error
+
+	doOnce.Do(func() {
+		log.Println("loading microsoft environment variables...")
+
+		MSClientID, err = loadVar("MS_CLIENT_ID")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		MSClientSecret, err = loadVar("MS_CLIENT_SECRET")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		MSRedirectURL, err = loadVar("MS_REDIRECT")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	})
+}
+
+func loadMongoDBVars() {
+	var err error
+
+	doOnce.Do(func() {
+		log.Println("loading mongodb atlas environment variables...")
+
+		MongoHost, err = loadVar("MONGO_HOST")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		MongoPassword, err = loadVar("MONGO_SECRET")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		MongoUser, err = loadVar("MONGO_USER")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	})
+}
+
+
+func loadVar(key string) (string, error) {
+	variable := os.Getenv(key)
+	if variable == "" {
+		return "", errors.New(fmt.Sprintf("%q environment variable not set!", key))
+	}
+
+	return variable, nil
+}
